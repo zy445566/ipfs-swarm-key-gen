fb698f0f4d6bd2d37c18a1d1f8535b0a83bd2fce
Matthew Bednarski <bednarski@walter-group.com>
2021-09-02 08:50:23 +0200

    fix(swarm-key): generate random bytes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const os = require('os')
+const crypto = require('crypto')
+
 exports = module.exports = function getSwarmKey(){
-    const buf = Buffer.allocUnsafe(32);
+    const buf = crypto.randomBytes(32); 
     return `/key/swarm/psk/1.0.0/${os.EOL}/base16/${os.EOL}${buf.toString('hex')}`;
 }
